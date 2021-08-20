be445f74360e0293054c808f3508bf84d99d627b
yeiipi <juanpab.sierra@urosario.edu.co>
2021-08-20 17:57:58 -0500

    Nuevas Funciones y N4
    AD:003

--- busqueda.py
+++ busqueda.py
@@ -1,3 +1,5 @@
+# ====| S E T U P |==== #
+import numpy as np
 from typing import List
 
 # ====| C L A S E S |==== #
@@ -24,6 +26,42 @@ def nodo_hijo(problema, madre: Nodo, accion: int) -> Nodo:
     costo_camino = madre.costo_camino + problema.costo(madre.estado, accion)
     codigo = problema.codigo(estado)
     return Nodo(estado, madre, accion, costo_camino, codigo)
+
+
+class ListaPrioritaria:
+    def __init__(self):
+        self.diccionario = {}
+
+    def __str__(self):
+        cadena = "["
+        inicial = True
+        for costo in self.diccionario:
+            elementos = self.diccionario[costo]
+            for elemento in elementos:
+                if inicial:
+                    cadena += "(" + str(elemento) + "," + str(costo) + ")"
+                    inicial = False
+                else:
+                    cadena += ", (" + str(elemento) + "," + str(costo) + ")"
+
+        return cadena + "]"
+
+    def push(self, elemento, costo):
+        try:
+            self.diccionario[costo].append(elemento)
+        except:
+            self.diccionario[costo] = [elemento]
+
+    def pop(self):
+        min_costo = np.min(np.array(list(self.diccionario.keys())))
+        candidatos = self.diccionario[min_costo]
+        elemento = candidatos.pop()
+        if len(candidatos) == 0:
+            del self.diccionario[min_costo]
+        return elemento
+
+    def is_empty(self):
+        return len(self.diccionario) == 0
 
 
 # ====| O T R A S |==== #
@@ -57,7 +95,7 @@ def EXPAND(problema, nodo: Nodo) -> List[Nodo]:
     s = nodo.estado
     nodos = list()
 
-    for ac in problema.acciones_aplicables(s):
+    for accion in problema.acciones_aplicables(s):
         hijo = nodo_hijo(problema, nodo, accion)
         nodos.append(hijo)
 
@@ -65,7 +103,82 @@ def EXPAND(problema, nodo: Nodo) -> List[Nodo]:
 
 
 # ====| B U S Q U E D A |==== #
-def depth_limited_search(problema, l:int) -> str:
-    pass
+
+# N2
+def depth_first_search(problema):
+    nodo = Nodo(problema.estado_inicial, None, None, 0, problema.estado_inicial)
+    if problema.test_objetivo(nodo.estado):
+        return nodo
+    frontera = [nodo]
+    explorados = []
+    while len(frontera) != 0:
+        nodo = frontera.pop()
+        explorados.append(nodo.codigo)
+        for accion in problema.acciones_aplicables(nodo.estado):
+            hijo = nodo_hijo(problema, nodo, accion)
+            if problema.test_objetivo(hijo.estado):
+                return hijo
+            if hijo.codigo not in explorados:
+                frontera.append(hijo)
+    return False
 
 
+# N2
+def breadth_first_search(problema):
+    nodo = Nodo(problema.estado_inicial, None, None, 0, problema.estado_inicial)
+    if problema.test_objetivo(nodo.estado):
+        return nodo
+    frontera = [nodo]
+    explorados = []
+    while len(frontera) != 0:
+        nodo = frontera[0]
+        del frontera[0]
+
+        explorados.append(nodo.codigo)
+        for accion in problema.acciones_aplicables(nodo.estado):
+            hijo = nodo_hijo(problema, nodo, accion)
+            if problema.test_objetivo(hijo.estado):
+                return hijo
+            if hijo.codigo not in explorados:
+                frontera.append(hijo)
+    return False
+
+
+# N3
+def depth_limited_search(problema, l: int) -> str:
+    nodo = Nodo(problema.estado_inicial, None, None, 0, problema.estado_inicial)
+    frontera = [nodo]
+    resultado = False
+
+    while len(frontera) != 0:
+        nodo = frontera.pop()
+        if problema.test_objetivo(nodo.estado):
+            return nodo
+        if depth(nodo) >= l:
+            pass
+
+
+# N3
+def best_first_search(problema, f=None):
+    if f is not None:
+        setattr(problema, "costo", f)
+    s = problema.estado_inicial
+    cod = problema.codigo(s)
+
+    nodo = Nodo(s, None, None, 0, cod)
+    frontera = ListaPrioritaria()
+    frontera.push(nodo, 0)
+    explorados = {}
+    explorados[cod] = 0
+    while not frontera.is_empty():
+        nodo = frontera.pop()
+        if problema.test_objetivo(nodo.estado):
+            return nodo
+        for hijo in EXPAND(problema, nodo):
+            s = hijo.estado
+            cod = problema.codigo(s)
+            c = hijo.costo_camino
+            if (cod not in explorados.keys()) or (c < explorados[cod]):
+                frontera.push(hijo, c)
+                explorados[cod] = c
+    return None
